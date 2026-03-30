62a3537c18cd6858f57729c5fe2bebf0875a72db
YamKai <eyadelgharably46@gmail.com>
2026-03-31 00:40:49 +0200

    remove useless user check

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,13 +1,9 @@
-function LandingPage({ onLogin, user }) {
+function LandingPage({ onLogin }) {
   return (
     <div>
         <h1>Welcome to Lectron</h1>
         <p>Your one-stop solution for complex coding tutorials.</p>
-      {user ? (
-        <button>Go to Dashboard</button>
-      ) : (
         <button onClick={onLogin}>Login with Google</button>
-      )}
     </div>
   );
 }
