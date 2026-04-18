32e90311d0d71703b3ff158c44e937cc4a185455
YamKai <eyadelgharably46@gmail.com>
2026-04-18 21:36:08 +0200

    fix bruh

--- client/src/components/main-dashboard/CourseCard.jsx
+++ client/src/components/main-dashboard/CourseCard.jsx
@@ -66,7 +66,7 @@ export default function CourseCard({
               </span>
 
               <div style={bar}>
-                <div style={ isCompleted ? { ...fillCompleted, width: `${progress}%` } : { ...fill, width: `${progress}%` }} />
+                <div style={ completed ? { ...fillCompleted, width: `${progress}%` } : { ...fill, width: `${progress}%` }} />
               </div>
             </div>
           )}
